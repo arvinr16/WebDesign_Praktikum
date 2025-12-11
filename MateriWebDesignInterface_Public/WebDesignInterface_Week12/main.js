6b5af7c57cc26bd4cca9a7baa3a0d290db8729a1
arvinr16 <arviansyahyk@gmail.com>
2025-12-11 22:09:39 +0700

    Adding 'materials' in 'MateriWebDesignInterface_Public'

---
+++ MateriWebDesignInterface_Public/WebDesignInterface_Week12/main.js
@@ -0,0 +1,5 @@
+console.log("Hello ini adalah menu");
+let menu1 = "Ayam Goreng";
+let menu2 = "Es Teh Manis";
+let hasil = menu1 + " + " + menu2;
+console.log("Jika ingin menikmati menu andalan kami maka anda musti memesan: ", hasil);
